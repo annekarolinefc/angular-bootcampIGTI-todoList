2a4c9f8913cc20f0d6bdda211d0d9ea98476aba6
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-19 09:15:01 -0300

    Exibindo e ocultando um Elemento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-estruturas-directivas';
+
+  exibir: boolean = true;
+  x: boolean = true;
+
+  toggleShow(){
+      this.exibir = !this.exibir;
+  }
+
 }
